f9b8501221a57156726718707829d63f445bc404
ninjaroot-509 <stanleycastin19@gmail.com>
2022-04-11 09:41:24 -0400

    update ninjaroot++

--- mizik/admin.py
+++ mizik/admin.py
@@ -52,9 +52,61 @@ class WalletRequestedAdmin(admin.ModelAdmin):
     search_fields = ['montant', 'user__first_name']
 admin.site.register(WalletRequested, WalletRequestedAdmin)
 
-admin.site.register(Album)
-admin.site.register(Song)
+class SongInline(admin.TabularInline):
+    """Tabular Inline View for Songs"""
+    model = Song
+
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = [
+        SongInline,
+    ]
+    list_display = ['artist', 'title', 'genre', 'get_count_songs']
+    list_filter = ['artist', 'genre']
+    search_fields = ['title', 'artist__first_name', 'artist__last_name']
+    def get_count_songs(self, obj):
+        a = Song.objects.filter(album=obj.id).count()
+        return a
+    get_count_songs.short_description = 'Nombres de musiques'
+admin.site.register(Album, AlbumAdmin)
+
 admin.site.register(ViewSong)
 admin.site.register(Like)
 admin.site.register(Download)
 admin.site.register(Genre)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'ordered',
+                    'coupon'
+                    ]
+    list_display_links = [
+        'user',
+        'coupon'
+    ]
+    list_filter = ['user',
+                   'ordered',]
+    search_fields = [
+        'user__first_name',
+        'ref_code'
+    ]
+
+admin.site.register(Order, OrderAdmin)
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'get_price',
+        'is_active'
+    ]
+    list_filter = ['title', 'is_active']
+    search_fields = ['title',]
+    def get_price(self, obj):
+        if obj.discount_price:
+            return obj.discount_price
+        return obj.price
+    get_price.short_description = 'Prix'
+admin.site.register(Song, SongAdmin)
+
+admin.site.register(OrderSong)
+admin.site.register(Coupon)
--- mizik/models.py
+++ mizik/models.py
@@ -80,6 +80,7 @@ class Album(models.Model):
     artist = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     title = models.CharField(max_length = 500)
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='albums/')
 
     def __str__(self):
@@ -90,6 +91,10 @@ class Song(models.Model):
     image = models.ImageField(upload_to='albums/', null=True, blank=True)
     file = models.FileField(upload_to='songs/')
     title = models.CharField(max_length = 100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
@@ -97,6 +102,71 @@ class Song(models.Model):
     
     def author(self):
         return self.album.artist.first_name + ' ' + self.album.artist.last_name
+    
+    def get_song_price(self):
+        return self.price
+
+    def get_discount_song_price(self):
+        return self.discount_price
+
+    def get_total_price(self):
+        if self.discount_price:
+            return self.get_discount_song_price()
+        return self.get_song_price()
+
+    def get_amount_saved(self):
+        return self.get_song_price() - self.get_discount_song_price()
+    
+class OrderSong(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.song.title}"
+
+    def get_total_song_price(self):
+        return self.song.price
+
+    def get_total_discount_song_price(self):
+        return self.song.discount_price
+
+    def get_amount_saved(self):
+        return self.get_total_song_price() - self.get_total_discount_song_price()
+
+    def get_final_price(self):
+        if self.song.discount_price:
+            return self.get_total_discount_song_price()
+        return self.get_total_song_price()
+    
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ref_code = models.CharField(max_length=20)
+    songs = models.ManyToManyField(OrderSong)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered = models.BooleanField(default=False)
+    ordered_date = models.DateTimeField()
+    coupon = models.ForeignKey('Coupon', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.first_name
+
+    def get_total(self):
+        total = 0
+        for order_song in self.songs.all():
+            total += order_song.get_final_price()
+        if self.coupon:
+            total -= self.coupon.amount
+        return total
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=15)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.code
+    
     
 class Like(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
